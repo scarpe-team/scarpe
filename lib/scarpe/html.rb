833e91d84cb80d2c9863c86f8de7aec4cacea417
Tobias Bales <tobias.bales@shopify.com>
2023-02-07 11:13:59 +0100

    Fix double ouput when multiple children are used in a block

--- lib/scarpe/html.rb
+++ lib/scarpe/html.rb
@@ -1,6 +1,6 @@
 module Scarpe
   class HTML
-    TAGS = %i[div p button]
+    TAGS = %i[div p button ul li]
 
     def self.render(&block)
       new(&block).value
@@ -26,7 +26,8 @@ module Scarpe
     def method_missing(name, *args, &block)
       raise NoMethodError, "no method #{name} for #{self.class.name}" unless TAGS.include?(name)
 
-      @buffer += "<#{name} #{render_attributes(*args)}>"
+      @buffer += "<#{name}#{render_attributes(*args)}>"
+
       if block_given?
         result = block.call(self)
         @buffer += result if result.is_a?(String)
@@ -35,14 +36,19 @@ module Scarpe
       end
 
       @buffer += "</#{name}>"
+
+      nil
     end
 
     private
 
     def render_attributes(attributes = {})
+      return "" if attributes.empty?
+
       attributes[:style] = render_style(attributes[:style]) if attributes[:style]
 
-      attributes.map { |k, v| "#{k}=\"#{v}\"" }.join(" ")
+      result = attributes.map { |k, v| "#{k}=\"#{v}\"" }.join(" ")
+      " #{result}"
     end
 
     def render_style(style)
