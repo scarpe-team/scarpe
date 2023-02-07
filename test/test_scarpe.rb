ae44e90bd681a3058ed8a8e572135ec874c2be9f
Tobias Bales <tobias.bales@shopify.com>
2023-02-07 10:29:12 +0100

    Remove failing assert false test and require dimensions
    This should fix all of the tests
    
    Co-authored-by: Calvin Walzel <calvin.walzel@shopify.com>

--- test/test_scarpe.rb
+++ test/test_scarpe.rb
@@ -6,8 +6,4 @@ class TestScarpe < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Scarpe::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
