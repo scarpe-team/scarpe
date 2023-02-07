341396b35dc674116252b725ae4775bdf38156ba
Drew Bragg <DRBragg@gmail.com>
2023-02-07 13:17:06 -0500

    docs: Add edit_box example

---
+++ examples/edit_box.rb
@@ -0,0 +1,12 @@
+require "scarpe"
+
+Scarpe.app do
+  para "Manuscript:"
+  @manuscript = edit_box(width: "100%") do
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim."
+  end
+  @char_count = para "#{@manuscript.text.length} characters"
+  @manuscript.change { |text|
+    @char_count.replace("#{text.length} characters")
+  }
+end
