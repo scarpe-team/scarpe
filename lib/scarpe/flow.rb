2264d065585a7a3f8094faa4c729d05b0cc4597e
Carlos Chitty <carlos.chitty@shopify.com>
2023-02-08 16:02:31 -0500

    Flow supports margin_left and margin_top

--- lib/scarpe/flow.rb
+++ lib/scarpe/flow.rb
@@ -2,9 +2,11 @@
 
 class Scarpe
   class Flow < Scarpe::Widget
-    def initialize(width: nil, margin: nil, &block)
+    def initialize(width: nil, margin: nil, margin_left: nil, margin_top: nil, &block)
       @width = width
       @margin = margin
+      @margin_left = margin_left
+      @margin_top = margin_top
       instance_eval(&block)
     end
 
@@ -23,6 +25,8 @@ class Scarpe
       styles["flex-direction"] = "row"
       styles["flex-wrap"] = "wrap"
       styles[:margin] = Dimensions.length(@margin) if @margin
+      styles["margin-left"] = Dimensions.length(@margin_left) if @margin_left
+      styles["margin-top"] = Dimensions.length(@margin_top) if @margin_top
       styles[:width] = Dimensions.length(@width) if @width
 
       styles
