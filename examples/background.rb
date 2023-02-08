ee8dac26dc5c3aa96fc7815690dea5041e2dec3d
Chris Waters <chris.waters@shopify.com>
2023-02-08 16:52:06 +0000

    wut

---
+++ examples/background.rb
@@ -0,0 +1,13 @@
+require "scarpe"
+
+Scarpe.app do
+  stack width: 0.33 do
+    background "#008C45"
+  end
+  stack width: 0.34 do
+    background "#F4F9FF"
+  end
+  stack width: 0.33 do
+    background "#CD212A"
+  end
+end
---
+++ lib/scarpe/background.rb
@@ -0,0 +1,10 @@
+class Scarpe
+  class Background < Scarpe::Widget
+    def initialize(decor)
+      @decor = decor
+    end
+
+    def element
+    end
+  end
+end
