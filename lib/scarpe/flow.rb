7b02af95ea74abe521d42ba9b9f8732fdef8b253
Carlos Chitty <carlos.chitty@shopify.com>
2023-02-08 15:13:18 -0500

    Let it flow

--- lib/scarpe/flow.rb
+++ lib/scarpe/flow.rb
@@ -1,7 +1,31 @@
+# frozen_string_literal: true
+
 class Scarpe
-    class Flow < Scarpe::Widget
-      def element
-        "<div style='display: flex; flex-direction: row' id='#{html_id}'></div>"
+  class Flow < Scarpe::Widget
+    def initialize(width: nil, margin: nil, &block)
+      @width = width
+      @margin = margin
+      instance_eval(&block)
+    end
+
+    def element(&block)
+      HTML.render do |h|
+        h.div(id: html_id, style:, &block)
       end
     end
+
+    private
+
+    def style
+      styles = {}
+
+      styles[:display] = "flex"
+      styles["flex-direction"] = "row"
+      styles["flex-wrap"] = "wrap"
+      styles[:margin] = Dimensions.length(@margin) if @margin
+      styles[:width] = Dimensions.length(@width) if @width
+
+      styles
+    end
   end
+end
