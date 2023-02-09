7babc4651ffd5a2116400d0c1e336ab3a25862bb
Gregorio Chalinski Kusowski <gregorio.kusowski@gmail.com>
2023-02-09 16:31:31 +0100

    Add more useful examples

---
+++ examples/coffee.rb
@@ -0,0 +1,16 @@
+require "scarpe"
+
+Scarpe.app title: "Sleepless", width: 80, height: 120 do
+  @push = button "☕️"
+  @note = para "😪"
+  @push.click {
+    if @pid.nil?
+      @pid = spawn("caffeinate -d")
+      @note.replace "😳"
+    else
+      Process.kill 9, @pid
+      @pid = nil
+      @note.replace "😪"
+    end
+  }
+end
