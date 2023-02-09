091495ef5c334ef0bd6c9496712af567a1b6013e
Andrew Novoselac <andrew.novoselac@shopify.com>
2023-02-09 10:37:53 -0500

    Improve specs

--- test/test_link.rb
+++ test/test_link.rb
@@ -11,8 +11,23 @@ class TestEditBox < Minitest::Test
 
   def test_link_with_a_block
     link = Scarpe::Link.new("click me") { "thanks" }
+    expected_attributes = {
+      id: link.html_id,
+      style: "color:blue",
+      onmouseover: "this.style.color='darkblue'",
+      onmouseout: "this.style.color='blue';",
+      onclick: link.handler_js_code("click")
+    }
 
-    assert_html link.to_html, :u, id: link.html_id, onclick: link.handler_js_code("click") do
+    assert_html link.to_html, :u, **expected_attributes do
+      "click me"
+    end
+  end
+
+  def test_link_with_a_url
+    link = Scarpe::Link.new("click me", click: "http://github.com/schwad/scarpe")
+
+    assert_html link.to_html, :a, href: "http://github.com/schwad/scarpe" do
       "click me"
     end
   end
