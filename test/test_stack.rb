5dd716054f368ad1ab1e61cc660bfd73803edc12
Drew Bragg <DRBragg@gmail.com>
2023-02-09 13:11:40 -0500

    test: Add some basic tests for spacing
    via stack tests

--- test/test_stack.rb
+++ test/test_stack.rb
@@ -10,4 +10,36 @@ class TestStack < Minitest::Test
 
     assert(stack.to_html.include?("height:25px"))
   end
+
+  def test_it_accepts_margin
+    stack = Scarpe::Stack.new(margin: 25) do
+      "fishes that flop"
+    end
+
+    assert(stack.to_html.include?("margin:25px"))
+  end
+
+  def test_it_accepts_margin_array
+    stack = Scarpe::Stack.new(margin: [1, 2, 3, 4]) do
+      "fishes that flop"
+    end
+
+    assert(stack.to_html.include?("margin-left:1px;margin-right:2px;margin-top:3px;margin-bottom:4px"))
+  end
+
+  def test_it_accepts_margin_hash
+    stack = Scarpe::Stack.new(margin: { left: 1, bottom: 4 }) do
+      "fishes that flop"
+    end
+
+    assert(stack.to_html.include?("margin-left:1px;margin-bottom:4px"))
+  end
+
+  def test_it_accepts_margin_keywords
+    stack = Scarpe::Stack.new(margin_left: 1, margin_bottom: 4) do
+      "fishes that flop"
+    end
+
+    assert(stack.to_html.include?("margin-left:1px;margin-bottom:4px"))
+  end
 end
