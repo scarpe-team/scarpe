3c403b204570a599951b1dc84200454509e11cdf
Andrew Novoselac <andrew.novoselac@shopify.com>
2023-02-10 16:46:41 -0500

    Tell CI to skip problematic examples

--- test/test_examples.rb
+++ test/test_examples.rb
@@ -4,7 +4,9 @@ require "test_helper"
 
 class TestScarpe < Minitest::Test
   def test_examples
-    examples_to_test = Dir["examples/**/*.rb"].reject { _1.include?("/not_checked/") }
+    examples_to_test = Dir["examples/**/*.rb"]
+      .reject { _1.include?("/not_checked/") }
+      .reject { _1.include?("/skip_ci/") if ENV["CI_RUN"] }
 
     puts "Testing #{examples_to_test.count} examples"
 
