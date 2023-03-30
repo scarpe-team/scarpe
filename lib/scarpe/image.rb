cc473ede2adba98e54ab69795b5460222f09d549
wee <85360080+gintama91@users.noreply.github.com>
2023-03-30 19:10:09 +0530

    image.size

--- lib/scarpe/image.rb
+++ lib/scarpe/image.rb
@@ -1,15 +1,26 @@
 # frozen_string_literal: true
 
+require "fastimage"
+require "open-uri"
 class Scarpe
   class Image < Scarpe::Widget
     display_properties :url, :width, :height, :top, :left, :click
 
     def initialize(url, width: nil, height: nil, top: nil, left: nil, click: nil)
       @url = url
-
       super
+      @width, @height = size(url)
 
       create_display_widget
+    end
+  end
+
+  class Widget
+    def size(url)
+      width, height = FastImage.size(url)
+
+      puts "Width is : #{width}"
+      puts "Height is : #{height}"
     end
   end
 end
