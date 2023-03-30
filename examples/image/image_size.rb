2f7a799043c8ca3408ff483b4f46bc9327a21159
wee <85360080+gintama91@users.noreply.github.com>
2023-03-30 14:02:39 +0000

    Revert "image.size"
    This reverts commit cc473ede2adba98e54ab69795b5460222f09d549.

--- examples/image/image_size.rb
+++
@@ -1,4 +0,0 @@
-Shoes.app do
-    size "https://images.unsplash.com/photo-1578632749014-ca77efd052eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGFuaW1lfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-end
-   
