eb092629854c46b2a330ca9718d191a963b03bfb
wee <85360080+gintama91@users.noreply.github.com>
2023-04-02 08:44:03 +0530

    removed .to_s and make sure sleepless.rb works cross platform without error

--- lib/scarpe/wv/web_wrangler.rb
+++ lib/scarpe/wv/web_wrangler.rb
@@ -621,19 +621,19 @@ class Scarpe
       end
 
       def value=(new_value)
-        @webwrangler.dom_change("document.getElementById('" + html_id.to_s + "').value = '" + new_value.to_s + "'; true")
+        @webwrangler.dom_change("document.getElementById('" + html_id + "').value = '" + new_value + "'; true")
       end
 
       def inner_text=(new_text)
-        @webwrangler.dom_change("document.getElementById('" + html_id.to_s + "').innerText = '" + new_text.to_s + "'; true")
+        @webwrangler.dom_change("document.getElementById('" + html_id + "').innerText = '" + new_text + "'; true")
       end
 
       def inner_html=(new_html)
-        @webwrangler.dom_change("document.getElementById(\"" + html_id.to_s + "\").innerHTML = `" + new_html.to_s + "`; true")
+        @webwrangler.dom_change("document.getElementById(\"" + html_id + "\").innerHTML = `" + new_html + "`; true")
       end
 
       def remove
-        @webwrangler.dom_change("document.getElementById('" + html_id.to_s + "').remove(); true")
+        @webwrangler.dom_change("document.getElementById('" + html_id + "').remove(); true")
       end
     end
   end
