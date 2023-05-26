ff3f81e65517d74187dea35b0e85a3f69a171af6
wee <85360080+gintama91@users.noreply.github.com>
2023-05-26 15:11:16 +0530

    forms example list_box

---
+++ lib/scarpe/list_box.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Scarpe
+    class ListBox < Scarpe::Widget
+      display_properties :selected_item, :items, :height, :width
+  
+      def initialize(args = {}, &block)
+        @items = args[:items] || []
+        @selected_item = args[:selected_item]
+        super()
+  
+        bind_self_event("change") do |new_item|
+          self.selected_item = new_item
+          @callback&.call(new_item)
+        end
+  
+        create_display_widget
+      end
+  
+      def change(&block)
+        @callback = block
+      end
+    end
+end
+  
--- lib/scarpe/wv/html.rb
+++ lib/scarpe/wv/html.rb
@@ -29,6 +29,36 @@ class Scarpe
       method_missing(:p, *args, &block)
     end
 
+    def option(**attrs, &block)
+      tag(:option, **attrs, &block)
+    end
+
+    def tag(name, **attrs, &block)
+      if VOID_TAGS.include?(name)
+        raise ArgumentError, "void tag #{name} cannot have content" if block_given?
+
+        @buffer += "<#{name}#{render_attributes(attrs)} />"
+      else
+        @buffer += "<#{name}#{render_attributes(attrs)}>"
+
+        if block_given?
+          result = block.call(self)
+        else
+          result = attrs[:content]
+          @buffer += result if result.is_a?(String)
+        end
+        @buffer += result if result.is_a?(String)
+
+        @buffer += "</#{name}>"
+      end
+
+      nil
+    end
+
+    def select(**attrs, &block)
+      tag(:select, **attrs, &block)
+    end
+
     def method_missing(name, *args, &block)
       raise NoMethodError, "no method #{name} for #{self.class.name}" unless TAGS.include?(name)
 
