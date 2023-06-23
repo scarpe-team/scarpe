8fe9c1cf90f849829d7661baf841ead26d5dcded
wee <85360080+gintama91@users.noreply.github.com>
2023-06-23 09:01:20 +0530

    adding color,padding to button

--- lib/scarpe/button.rb
+++ lib/scarpe/button.rb
@@ -2,11 +2,14 @@
 
 class Scarpe
   class Button < Scarpe::Widget
-    display_properties :text, :width, :height, :top, :left
+    display_properties :text, :width, :height, :top, :left, :color, :padding_top, :padding_bottom, :text_color, :font_size
 
-    def initialize(text, width: nil, height: nil, top: nil, left: nil, &block)
+    def initialize(text, width: nil, height: nil, top: nil, left: nil, color: nil, padding_top: nil, padding_bottom: nil, text_color: nil,
+      font_size: nil, & block)
       # Properties passed as positional args, not keywords, don't get auto-set
       @text = text
+      @color = color
+
       @block = block
 
       super
