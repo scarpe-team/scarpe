3e16bbb2531c8e65d1d010227bb0f7b2e93a38bf
gintama91 <b9-20@srkrec.edu.in>
2023-07-07 19:21:01 +0530

    added various video formats

--- lib/scarpe/wv/video.rb
+++ lib/scarpe/wv/video.rb
@@ -2,6 +2,14 @@
 
 class Scarpe
   class WebviewVideo < Scarpe::WebviewWidget
+    SUPPORTED_FORMATS = {
+      "video/mp4" => [".mp4"],
+      "video/webp" => [".webp"],
+      "video/quicktime" => [".mov"],
+      "video/x-matroska" => [".mkv"],
+      # Add more formats and their associated file extensions if needed
+    }.freeze
+
     def initialize(properties)
       @url = properties[:url]
       super
@@ -10,17 +18,23 @@ class Scarpe
     def element
       HTML.render do |h|
         h.video(id: html_id, style: style, controls: true) do
-          h.source(src: @url, type: "video/mp4")
+          supported_formats.each do |format|
+            h.source(src: @url, type: format)
+          end
         end
       end
     end
 
     private
 
+    def supported_formats
+      SUPPORTED_FORMATS.select { |_format, extensions| extensions.include?(File.extname(@url)) }.keys
+    end
+
     def style
       styles = {}
 
-      #ADD YOUR STYLES HERE
+      # ADD YOUR STYLES HERE
 
       styles.compact
     end
