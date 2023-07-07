9dac797ab7945a7542f003f442ddce2e41cf1659
gintama91 <b9-20@srkrec.edu.in>
2023-07-07 16:57:40 +0530

    background with image

---
+++ examples/backdround_with_image.rb
@@ -0,0 +1,5 @@
+Shoes.app do
+  stack width:200, height:400 do
+    background "docs/static/avatar.png"
+  end
+end
--- lib/scarpe/wv/background.rb
+++ lib/scarpe/wv/background.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
+require_relative "../base64"
 class Scarpe
   module WebviewBackground
+    include Base64
     def style
       styles = (super if defined?(super)) || {}
       return styles unless @background_color
 
       color = if @background_color.is_a?(Range)
         "linear-gradient(45deg, #{@background_color.first}, #{@background_color.last})"
+      elsif File.exist?(@background_color)
+        # @background_color is a valid file path
+        "url(data:image/png;base64,#{encode_file_to_base64(@background_color)})"
       else
         @background_color
       end
