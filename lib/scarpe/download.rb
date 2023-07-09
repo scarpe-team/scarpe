1ea1c84f6c42c3eab5f511756ae80ce13b057ff4
gintama91 <b9-20@srkrec.edu.in>
2023-07-09 10:30:59 +0530

    download get_google and save

--- lib/scarpe/download.rb
+++ lib/scarpe/download.rb
@@ -1,47 +1,87 @@
 # frozen_string_literal: true
 
-require "net/http"
-require "openssl"
-require "nokogiri"
+class Scarpe
+  class Widget
+    def download(url, method: "GET", save: nil, styles: {}, &block)
+      @block = block
 
-class Scarpe::Widget
-  def download(url)
-    Thread.new do
-      begin
-        uri = URI(url)
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      Thread.new do
+        logger = ::Scarpe::Logger.logger("Scarpe::App#download")
+        begin
+          uri = URI(url)
+          port = uri.port || (uri.scheme == "https" ? 443 : 80)
+          http = Net::HTTP.start(uri.host, port, use_ssl: uri.scheme == "https", verify_mode: OpenSSL::SSL::VERIFY_NONE)
 
-        request = Net::HTTP::Get.new(uri.request_uri)
+          request = Net::HTTP.const_get(method.capitalize).new(uri.request_uri)
+          apply_styles(request, styles)
 
-        http.request(request) do |response|
-          case response
-          when Net::HTTPSuccess
-            # content = response.body
-
-            # headers = response.header
-
-            html_get_title(content)
-          else
-            Scarpe.error("Failed to download content. Response code: #{response.code}")
+          http.request(request) do |response|
+            handle_response(response, save, styles)
           end
+        rescue Net::HTTPError, Net::OpenTimeout, Net::ReadTimeout => e
+          handle_error(e, logger)
+        rescue StandardError => e
+          handle_error(e, logger)
         end
-      rescue StandardError => e
-        Scarpe.error("Error occurred while downloading: #{e.message}")
       end
     end
-  end
 
-  private
+    private
 
-  def html_get_title(content)
-    doc = Nokogiri::HTML(content)
+    def apply_styles(request, styles)
+      headers = styles[:headers]
+      request["Content-Type"] = headers["Content-Type"] if headers && headers["Content-Type"]
+      request.body = styles[:body] if styles[:body]
+      # ah ig yes add more custom styles to the request as needed
+    end
 
-    title = doc.at_css("title")&.text&.strip || ""
+    def handle_response(response, save, styles)
+      case response
+      when Net::HTTPSuccess
+        content = response.body
+        headers = extract_headers(response)
+        if save
+          save_content(content, save)
+        else
+          handle_finish_event(headers)
+        end
+      else
+        handle_failure(response.code)
+      end
+    end
 
-    # headings = doc.css("h1").map(&:text)
+    class FileError < StandardError
+    end
 
-    title
+    def save_content(content, file_name)
+      file = nil
+      begin
+        file = File.open(file_name, "w")
+        file.write(content)
+      rescue Errno::EACCES, Errno::ENOENT => e
+        raise FileError, "File error occurred: #{e.message}"
+      else
+        # Code to execute when no exceptions or errors occur
+        @block&.call
+      ensure
+        file&.close
+      end
+    end
+
+    def handle_finish_event(headers)
+      @block&.call(headers)
+    end
+
+    def extract_headers(response)
+      response.each_header.to_h.to_s
+    end
+
+    def handle_failure(code)
+      logger.error("Failed to download content. Response code: #{code}")
+    end
+
+    def handle_error(error, logger)
+      logger.error("An error occurred while downloading: #{error.message}")
+    end
   end
 end
