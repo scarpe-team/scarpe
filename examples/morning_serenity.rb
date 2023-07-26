21c0da36f15f2933627ce8a74d3c0c8839f95312
Nick Schwaderer <nick.schwaderer@shopify.com>
2023-07-26 08:46:09 +0100

    Morning serenity bloops song to wake people up and get their hackdays started right
    Signed-off-by: Nick Schwaderer <nick.schwaderer@shopify.com>

---
+++ examples/morning_serenity.rb
@@ -0,0 +1,27 @@
+require 'shoes'
+require 'bloops'
+
+Shoes.app height: 200, width: 200, title: "Morning Serenity" do
+  @sleepy_person = para "😴"
+
+  button "Good Morning!" do
+    @sleepy_person.replace "😀"
+    b = Bloops.new
+    b.tempo = 70
+
+    s1 = b.sound Bloops::SINE
+    s1.attack = 0.5
+    s1.sustain = 0.3
+    s1.decay = 0.4
+
+    pattern1 = "4:e5 4:d5 4:c5 4:d5 2:e5 4:e5 4:d5 4:c5 4:d5 2:e5 4:d5 4:c5 4:b4 4:c5 2:d5"
+
+    b.tune s1, pattern1
+
+    Thread.new do
+      b.play
+      sleep 1 while !b.stopped?
+    end
+  end
+
+end
