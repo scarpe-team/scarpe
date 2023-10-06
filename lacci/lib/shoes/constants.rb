9700980ab4122799df19ffe498cfdc66049e6fa9
gintama91 <pavannambi0408@gmail.com>
2023-10-06 20:41:35 +0530

    fetch release name and build type from changelog

--- lacci/lib/shoes/constants.rb
+++ lacci/lib/shoes/constants.rb
@@ -14,6 +14,7 @@ module Shoes
         [ENV["HOME"], ".shoes"],
         [Dir.tmpdir, "shoes"],
       ]
+
       top, file = homes.detect { |home_top, _| home_top && File.exist?(home_top) }
       File.join(top, file)
     end
@@ -25,5 +26,19 @@ module Shoes
     TWO_PI = 6.28318530717958647693
     HALF_PI = 1.57079632679489661923
     PI = 3.14159265358979323846
+  end
+
+  changelog_content = File.read("CHANGELOG.md")
+
+  release_name_pattern = /^## \[(\d+\.\d+\.\d+)\] - (\d{4}-\d{2}-\d{2})$/m
+
+  release_matches = changelog_content.scan(release_name_pattern)
+
+  latest_release = release_matches.max_by { |version, _date| Gem::Version.new(version) }
+
+  if latest_release
+    RELEASE_NAME, RELEASE_BUILD_DATE = latest_release
+  else
+    puts "most likely something wrong in constants.rb file. or changelog"
   end
 end
