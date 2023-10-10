530db55144f9456d9a61834efb6c2220d6ab77f8
Noah Gibbs <the.codefolio.guy@gmail.com>
2023-10-10 11:34:53 +0100

    Remove puts on every run and failure when run outside Scarpe dir

--- lacci/lib/shoes/changelog.rb
+++ lacci/lib/shoes/changelog.rb
@@ -15,22 +15,24 @@ module Shoes
     end
 
     def get_latest_release_info
-      changelog_content = File.read("CHANGELOG.md")
+      changelog_dir = File.dirname(__FILE__, 4) # this duplicates constants.rb, but how to share?
+      changelog_file = "#{changelog_dir}/CHANGELOG.md"
 
-      release_name_pattern = /^## \[(\d+\.\d+\.\d+)\] - (\d{4}-\d{2}-\d{2})$/m
+      if File.exist?(changelog_file)
+        changelog_content = File.read(changelog_file)
+        release_name_pattern = /^## \[(\d+\.\d+\.\d+)\] - (\d{4}-\d{2}-\d{2})$/m
+        release_matches = changelog_content.scan(release_name_pattern)
+        latest_release = release_matches.max_by { |version, _date| Gem::Version.new(version) }
 
-      release_matches = changelog_content.scan(release_name_pattern)
-
-      latest_release = release_matches.max_by { |version, _date| Gem::Version.new(version) }
-
-      if latest_release
-        puts "Found release #{latest_release[0]} in CHANGELOG.md"
-        # @log.debug("Found release #{latest_release[0]} in CHANGELOG.md")
-        { RELEASE_NAME: latest_release[0], RELEASE_BUILD_DATE: latest_release[1] }
-      else
-        puts "No release found in CHANGELOG.md"
-        { RELEASE_NAME: nil, RELEASE_BUILD_DATE: nil }
+        if latest_release
+          #puts "Found release #{latest_release[0]} in CHANGELOG.md"
+          # @log.debug("Found release #{latest_release[0]} in CHANGELOG.md") # Logger isn't initialized yet
+          return({ RELEASE_NAME: latest_release[0], RELEASE_BUILD_DATE: latest_release[1] })
+        end
       end
+
+      puts "No release found in CHANGELOG.md"
+      { RELEASE_NAME: nil, RELEASE_BUILD_DATE: nil }
     end
   end
 end
