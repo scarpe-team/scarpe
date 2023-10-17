4c6e2d435d61a2f25b9e6da43545b2d1ac2fe93f
gintama91 <pavannambi0408@gmail.com>
2023-10-17 13:51:52 +0530

    code

---
+++ lib/scarpe/wv/codes.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Scarpe::Webview
+  class Codes < Drawable
+    def initialize(properties)
+      super
+    end
+
+    def element
+      render("codes")
+    end
+  end
+end
