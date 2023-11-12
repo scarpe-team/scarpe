af275f9913e14c6c50eeb608dd66082f037f190e
BLSowmya <112118575+BLSowmya@users.noreply.github.com>
2023-11-12 19:06:58 +0530

    Update builtins.rb

--- lacci/lib/shoes/builtins.rb
+++ lacci/lib/shoes/builtins.rb
@@ -8,55 +8,53 @@ module Shoes::Builtins
   #
   # @param font_file_or_url [String] the filename or URL for the font
   # @return [void]
-  class Shoes::Builtins
-    def font(font_file_or_url)
-      shoes_builtin("font", font_file_or_url)
+  def font(font_file_or_url)
+    shoes_builtin("font", font_file_or_url)
 
-      font_name = File.basename(font_file_or_url, ".*")
-      Shoes::FONTS << font_name
-    end
+    font_name = File.basename(font_file_or_url, ".*")
+    Shoes::FONTS << font_name
+  end
 
-    def ask(message_string)
-      shoes_builtin("ask", message_string)
-    end
+  def ask(message_string)
+    shoes_builtin("ask", message_string)
+  end
 
-    def alert(message)
-      shoes_builtin("alert", message)
-    end
+  def alert(message)
+    shoes_builtin("alert", message)
+  end
 
-    def ask_color(title_bar)
-      shoes_builtin("ask_color", title_bar)
-    end
+  def ask_color(title_bar)
+    shoes_builtin("ask_color", title_bar)
+  end
 
-    def ask_open_file
-      shoes_builtin("ask_open_file")
-    end
+  def ask_open_file()
+    shoes_builtin("ask_open_file")
+  end
 
-    def ask_save_file
-      shoes_builtin("ask_save_file")
-    end
+  def ask_save_file()
+    shoes_builtin("ask_save_file")
+  end
 
-    def ask_open_folder
-      shoes_builtin("ask_open_folder")
-    end
+  def ask_open_folder()
+    shoes_builtin("ask_open_folder")
+  end
 
-    def ask_save_folder
-      shoes_builtin("ask_save_folder")
-    end
+  def ask_save_folder()
+    shoes_builtin("ask_save_folder")
+  end
 
-    def confirm(question)
-      shoes_builtin("confirm", question)
-    end
+  def confirm(question)
+    shoes_builtin("confirm", question)
+  end
 
-    # TO ADD: debug, error, info, warn
-    # TO VERIFY OR ADD: gradient, gray, rgb
+  # TO ADD: debug, error, info, warn
+  # TO VERIFY OR ADD: gradient, gray, rgb
 
-    private
+  private
 
-    def shoes_builtin(cmd_name, *args)
-      Shoes::DisplayService.dispatch_event("builtin", nil, cmd_name, args)
-      nil
-    end
+  def shoes_builtin(cmd_name, *args)
+    Shoes::DisplayService.dispatch_event("builtin", nil, cmd_name, args)
+    nil
   end
 end
 
